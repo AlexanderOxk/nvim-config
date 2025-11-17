3bfdb23fddac758106c85ac9fbcbd33d994828e0
Alexander Oxklint <alex.oxklint@gmail.com>
2025-11-17 20:52:30 +0100

    Add multi-grep picker for telescope

--- lua/my/plugins/telescope.lua
+++ lua/my/plugins/telescope.lua
@@ -40,42 +40,6 @@ return {
 
         local builtin = require("telescope.builtin")
 
-        -- Telescope live_grep in git root
-        -- Function to find the git root directory based on the current buffer's path
-        local function find_git_root()
-            -- Use the current buffer's path as the starting point for the git search
-            local current_file = vim.api.nvim_buf_get_name(0)
-            local current_dir
-            local cwd = vim.fn.getcwd()
-            -- If the buffer is not associated with a file, return nil
-            if current_file == "" then
-                current_dir = cwd
-            else
-                -- Extract the directory from the current file's path
-                current_dir = vim.fn.fnamemodify(current_file, ":h")
-            end
-
-            -- Find the Git root directory from the current file's path
-            local git_root = vim.fn.systemlist("git -C " ..
-                vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
-            if vim.v.shell_error ~= 0 then
-                print("Not a git repository. Searching on current working directory")
-                return cwd
-            end
-            return git_root
-        end
-
-        --
-        -- Custom live_grep function to search in git root
-        local function live_grep_git_root()
-            local git_root = find_git_root()
-            if git_root then
-                builtin.live_grep({
-                    search_dirs = { git_root },
-                })
-            end
-        end
-
         --
         -- Custom function to smart-select git_files or find_files
         local function find_git_files_or_files()
@@ -90,8 +54,6 @@ return {
             end
         end
 
-
-        vim.api.nvim_create_user_command('LiveGrepGitRoot', live_grep_git_root, {})
         vim.api.nvim_create_user_command('FindGitFilesOrFiles', find_git_files_or_files, {})
 
         vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
@@ -109,8 +71,10 @@ return {
         vim.keymap.set('n', '<leader>ph', builtin.help_tags, { desc = '[ph] Search help' })
         vim.keymap.set('n', '<leader>pq', builtin.quickfix, { desc = '[pq] Search quickfix' })
         vim.keymap.set('n', '<leader>pw', builtin.grep_string, { desc = '[pw] Search current word' })
-        vim.keymap.set('n', '<leader>pg', ':LiveGrepGitRoot<cr>', { desc = '[pg] Search by grep on Git Root' })
         vim.keymap.set('n', '<leader>pd', builtin.diagnostics, { desc = '[pd] Search diagnostics' })
         vim.keymap.set('n', '<leader>pr', builtin.resume, { desc = '[pr] Search resume' })
+
+        -- Custom pickers
+        vim.keymap.set("n", "<leader>pg", require("my.telescope.multigrep").live_multigrep, { desc = "[pg] Multi Grep" })
     end
 }
