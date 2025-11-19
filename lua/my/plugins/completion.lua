94fc528fb32de488b9af77f10acb025a3ffe48d6
Alexander Oxklint <alex.oxklint@gmail.com>
2025-11-19 21:32:27 +0100

    Split snippet and completion plugin configs to two specs

--- lua/my/plugins/completion.lua
+++ lua/my/plugins/completion.lua
@@ -12,36 +12,12 @@ return {
         },
         config = function()
             local cmp = require('cmp')
-            local ls = require('luasnip')
-            local opts = { noremap = true, silent = true }
-
-            ls.setup()
-            require('luasnip.loaders.from_vscode').lazy_load()
-            require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/submodules/friendly-snippets" } })
-
-            -- Use <C-j> <C-k> to jump in snippets
-            vim.keymap.set({ "i", "s" }, "<C-j>", function() ls.jump(1) end, opts)
-            vim.keymap.set({ "i", "s" }, "<C-k>", function() ls.jump(-1) end, opts)
-
-
-            -- Use <C-l> <C-h> to select choice in snippet
-            vim.keymap.set({ "i", "s" }, "<C-l>", function()
-                if ls.choice_active() then
-                    ls.change_choice(1)
-                end
-            end, opts)
-
-            vim.keymap.set({ "i", "s" }, "<C-h>", function()
-                if ls.choice_active() then
-                    ls.change_choice(-1)
-                end
-            end, opts)
 
             cmp.setup(
                 {
                     snippet = {
                         expand = function(args)
-                            ls.lsp_expand(args.body)
+                            require('luasnip').lsp_expand(args.body)
                         end,
                     },
                     completion = {
---
+++ lua/my/plugins/snippets.lua
@@ -0,0 +1,31 @@
+return {
+    'L3MON4D3/LuaSnip',
+    config = function()
+        local ls = require('luasnip')
+        local opts = { noremap = true, silent = true }
+
+        ls.setup()
+        require('luasnip.loaders.from_vscode').lazy_load()
+        require("luasnip.loaders.from_vscode").lazy_load({
+            paths = { vim.fn.stdpath("config") .. "/submodules/friendly-snippets" }
+        })
+
+        -- Use <C-j> <C-k> to jump in snippets
+        vim.keymap.set({ "i", "s" }, "<C-j>", function() ls.jump(1) end, opts)
+        vim.keymap.set({ "i", "s" }, "<C-k>", function() ls.jump(-1) end, opts)
+
+
+        -- Use <C-l> <C-h> to select choice in snippet
+        vim.keymap.set({ "i", "s" }, "<C-l>", function()
+            if ls.choice_active() then
+                ls.change_choice(1)
+            end
+        end, opts)
+
+        vim.keymap.set({ "i", "s" }, "<C-h>", function()
+            if ls.choice_active() then
+                ls.change_choice(-1)
+            end
+        end, opts)
+    end
+}
