a7791eda1ff90c61420438f24609dd5ccf952987
Alexander Oxklint <alex.oxklint@gmail.com>
2025-11-19 09:59:24 +0100

    Add groups to autocommands

--- lua/my/core/autocommands.lua
+++ lua/my/core/autocommands.lua
@@ -1,25 +1,33 @@
 -- Highlight Yanked Text
-vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = 'Highlight yanked text',
+    group = vim.api.nvim_create_augroup('my-highlight-yank', { clear = true }),
     callback = function()
-        vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+        vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
     end,
 })
 
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
+vim.api.nvim_create_autocmd("BufEnter", {
+    desc = 'Recognize bashrc files as bash filetype',
+    group = vim.api.nvim_create_augroup('my-bash-file-detection', { clear = true }),
     pattern = { ".bashrc.user", ".bashrc" },
     callback = function()
-        vim.cmd 'set filetype=bash'
+        vim.cmd('set filetype=bash')
     end
 })
 
 -- Highlight trailing spaces red
 vim.api.nvim_set_hl(0, "ExtraWhitespace", { bg = "red" })
 vim.api.nvim_create_autocmd({ "BufWinEnter", "InsertLeave" }, {
+    desc = 'Highlight trailing spaces as red',
+    group = vim.api.nvim_create_augroup('my-trailing-whitespace-1', { clear = true }),
     callback = function()
         vim.fn.matchadd("ExtraWhitespace", [[\s\+$]])
     end,
 })
 vim.api.nvim_create_autocmd("InsertEnter", {
+    desc = 'Highlight trailing spaces as red',
+    group = vim.api.nvim_create_augroup('my-trailing-whitespace-2', { clear = true }),
     callback = function()
         vim.fn.matchadd("ExtraWhitespace", [[\s\+\%#\@<!$]])
     end,
